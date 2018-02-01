75a8ea9b6e23c9f4b93ea9b1c45d8e673b123394
robpierini <r.pierini1993@gmail.com>
2018-02-01 10:44:50 -0800

    added addtag file

--- controllers/tags/addtag.js
+++ controllers/tags/addtag.js
@@ -6,81 +6,28 @@ const Schema = require('../../lib/schema');
 const swagger = Schema.generate();
 
 module.exports = {
-    description: 'Add item',
-    tags: ['api', 'users'],
+    description: 'Add tags',
+    tags: ['api', 'mod'],
     validate: {
-        payload: Schema.additem,
+        payload: Joi.object({'name': Joi.string().required()}),
         headers: Joi.object({
             'authorization': Joi.string().required()
         }).unknown()
     },
     handler: async function (request, reply) {
-
         const credentials = request.auth.credentials;
+        if( await this.db.tags.findOne(request.payload) ){
+            throw Boom.conflict(`Username ${request.payload} already exists`);
+        }
+        await this.db.tags.insert(request.payload);
 
-        if (!request.payload.name) {
-            throw Boom.badRequest("No name given");
-        }
-        else if (request.payload.type != "place" && request.payload.type != "group" && request.payload.type != "activity" && request.payload.type != "event") {
-            throw Boom.badRequest("Invalid type");
-        }
-
-        if (request.payload.type != "event") {
-            if (!request.payload.start_date && !request.payload.end_date) { }
-            else {
-                throw Boom.badRequest("Only event can have start and end dates")
-            }
-        }
-        else {
-            if (!request.payload.start_date) {
-                throw Boom.badRequest("Event must have a start date")
-            }
-        }
-        let returneditem;
-
-        if (request.payload.type == "place") {
-            //error checking
-            if (!request.payload.linked_place) { }
-            else {
-                throw Boom.badRequest("Can't link to place")
-            }
-            if (!request.payload.linked_group) { }
-            else {
-                throw Boom.badRequest("Can't link to group")
-            }
-
-        }
-        else if (request.payload.type == "activity") {
-            //error checking
-            if (!request.payload.linked_group) { }
-            else {
-                throw Boom.badRequest("Can't link to group")
-            }
-        }
-        else if (request.payload.type == "group") {
-            //error checking
-            if (!request.payload.linked_group) { }
-            else {
-                throw Boom.badRequest("Can't link to group")
-            }
-        }
-        else { //event
-            //error checking
-            if (!request.payload.linked_place) {
-                throw Boom.badRequest("No place linked to event")
-            }
-        }
-        returneditem = await this.db.items.insert(request.payload);
-
-        await this.db.item_owners.insert({ item_id: returneditem.id, user_id: credentials.id });
-
-        return reply({ data: returneditem });
+        return reply({message: 'Tag added successfully'});
     },
-    response: {
-        status: {
-            200: Schema.item_response
-        }
-    },
+    // response: {
+    //     status: {
+    //         200: Schem
+    //     }
+    // },
     plugins: {
         'hapi-swagger': swagger
     }
