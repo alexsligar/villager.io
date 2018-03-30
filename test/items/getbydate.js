b4ea8a4895d118c5ffa19a02ef96e228c8351710
lvancleef <13970250+lvancleef@users.noreply.github.com>
2018-03-30 15:12:45 -0700

    Test items/getbydate

---
+++ test/items/getbydate.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Fixtures = require('../fixtures');
+const Server = Fixtures.server;
+const db = Fixtures.db;
+
+const { after, before, describe, it } = exports.lab = require('lab').script();
+const { expect } = require('code');
+
+
+describe('GET /items/days/{days}', () => {
+
+    let server;
+    const event = Fixtures.event();
+    let newEvent;
+
+    before(async () => {
+
+        server = await Server;
+
+        newEvent = await Promise.all([
+            db.items.insert(event)
+        ]);
+    });
+
+    after(async () => {
+
+        await Promise.all([
+            db.items.destroy({ id: newEvent[0].id })
+        ]);
+    });
+
+    it('Get Item', async () => {
+
+        const query = {
+            method: 'GET',
+            url: `/items/days/1`
+        };
+        const response = await server.inject(query);
+        expect(response.statusCode).to.equal(200);
+    });
+});
