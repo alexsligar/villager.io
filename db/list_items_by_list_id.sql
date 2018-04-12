711cbfc48ddc09294cf14434582fae69567c83be
lvancleef <13970250+lvancleef@users.noreply.github.com>
2018-04-12 12:14:57 -0700

    Updated List controllers, Query. Query no longer returns tags, no links

--- db/list_items_by_list_id.sql
+++ db/list_items_by_list_id.sql
@@ -1,8 +1,5 @@
-SELECT
-items.*,
-array_agg(item_tags.tag_name) as tags
+SELECT items.*
 FROM list_items
 left JOIN items ON list_items.item_id = items.id
-LEFT JOIN item_tags ON items.id = item_tags.item_id
 WHERE list_id = $(id)
 Group by items.id
