9de6db9d495ce05598cced215c9cfe32303dba58
Alex Sligar <info@gravelroad.co>
2019-01-10 11:13:30 -0800

    Add lists items to response

--- db/lists_getAllByOwner.sql
+++ db/lists_getAllByOwner.sql
@@ -1,8 +1,9 @@
 SELECT 
 lists.id,
 lists.name,
-lists.description
+lists.description,
+array_agg(list_items.item_id) as items
 FROM lists
+LEFT JOIN list_items ON lists.id = list_items.list_id
 WHERE lists.owner = $(owner)
-AND
-lists.owner != lists.id
+GROUP BY lists.id
