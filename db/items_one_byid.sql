742a96a0e29296127ad8a9e74ae3ab781ea7dfe6
Alex Sligar <info@gravelroad.co>
2019-01-10 11:00:17 -0800

    Add item_owners to the item response

--- db/items_one_byid.sql
+++ db/items_one_byid.sql
@@ -1,5 +1,6 @@
-Select items.*, array_agg(item_tags.tag_name) as tags
+Select items.*, array_agg(distinct item_tags.tag_name) as tags, array_agg(distinct item_owners.username) as owners
 From items 
 left join item_tags on items.id = item_tags.item_id
+left join item_owners on items.id = item_owners.item_id
 WHERE items.id = ${id}
 Group by items.id
