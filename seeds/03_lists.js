b8e2b868fb998b65b8e258077f2c3d040a76534c
Alex Sligar <info@gravelroad.co>
2019-01-10 11:17:19 -0800

    Fix list seeds to use username for ownership

--- seeds/03_lists.js
+++ seeds/03_lists.js
@@ -8,28 +8,28 @@ exports.seed = function seed( knex, Promise ) {
         {
             id: '1a9d09e7-a27e-414d-9417-01ea01669a59',
             name: 'Nerd clubs',
-            owner: 'e129cc9e-460e-4e52-9573-92e3ff0007ef',
+            owner: 'Robfrikaans',
             description: 'I am very creative'
         },
         {
             id: 'e129cc9e-460e-4e52-9573-92e3ff0007ef',
             name: 'starred',
-            owner: 'e129cc9e-460e-4e52-9573-92e3ff0007ef'
+            owner: 'Randolphin'
         },
         {
             id: 'c06f747f-81cd-4033-8752-d284cd1c1706',
             name: 'starred',
-            owner: 'c06f747f-81cd-4033-8752-d284cd1c1706'
+            owner: 'modérateur'
         },
         {
             id: '269fffd8-a550-4871-8af2-db6eda3d6fb4',
             name: 'Best Restaurants',
-            owner: '8865b22c-a732-4381-9ba6-e9bc32fc9b99'
+            owner: 'Robfrikaans'
         },
         {
             id: '8865b22c-a732-4381-9ba6-e9bc32fc9b99',
             name: 'starred',
-            owner: '8865b22c-a732-4381-9ba6-e9bc32fc9b99'
+            owner: 'Robfrikaans'
         }
 
     ];
