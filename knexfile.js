bb9ceecff95bcf246ad2d55f9a52b1bddba5b523
Alex Sligar <info@gravelroad.co>
2019-01-14 13:02:33 -0800

    Clean up to match styleguide

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,10 @@
-// knex cli looks to this file
 'use strict';
+// knex cli looks to this file
 
 const Config = require('getconfig');
+
 module.exports = Config.db;
+
 if (Config.getconfig.env !== 'production') {
     module.exports[Config.getconfig.env] = Config.db;
 }
