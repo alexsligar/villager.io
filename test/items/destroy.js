475034da905e72a2ba645c8911a8a03fcde0d0c7
Alex Sligar <info@gravelroad.co>
2019-01-15 12:10:36 -0800

    Clean up tests so they aren't coupled and db is cleaned

--- test/items/destroy.js
+++ test/items/destroy.js
@@ -3,172 +3,135 @@
 const JWT = require('jsonwebtoken');
 const Config = require('getconfig');
 const Fixtures = require('../fixtures');
+const Faker = require('faker');
+
 const Server = Fixtures.server;
 const db = Fixtures.db;
 
-const { after, before, describe, it } = exports.lab = require('lab').script();
+const { after, before, beforeEach, describe, it } = exports.lab = require('lab').script();
 const { expect } = require('code');
 
 
 describe('DELETE /items/{id}', () => {
 
     let server;
-
     const user = Fixtures.user_id();
-    const invalidUser = Fixtures.user_id();
-    const admin = Fixtures.user_admin();
-
-    const event = Fixtures.event();
-    const multiEvent = Fixtures.event();
-    const listEvent = Fixtures.event();
-
-    const list = Fixtures.list();
-
-    let newEvent;
-    let newMultiEvent;
-    let newListEvent;
-    let newList;
+    const userTwo = Fixtures.user();
+    const adminUser = Fixtures.user_admin();
+    let token;
+    let query;
 
     before(async () => {
 
+        await Promise.all([
+            db.users.insert(user),
+            db.users.insert(userTwo),
+            db.users.insert(adminUser)
+        ]);
         server = await Server;
-        newEvent = await Promise.all([
-            db.items.insert(event)
-        ]);
-        newMultiEvent = await Promise.all([
-            db.items.insert(multiEvent)
-        ]);
-        newListEvent = await Promise.all([
-            db.items.insert(listEvent)
-        ]);
-        await Promise.all([
-            db.users.insert(user)
-        ]);
-        await Promise.all([
-            db.item_owners.insert({ username: user.username, item_id: newEvent[0].id })
-        ]);
-        await Promise.all([
-            db.users.insert(invalidUser)
-        ]);
-        await Promise.all([
-            db.item_owners.insert({ username: user.username, item_id: newMultiEvent[0].id })
-        ]);
-        await Promise.all([
-            db.item_owners.insert({ username: invalidUser.username, item_id: newMultiEvent[0].id })
-        ]);
-        await Promise.all([
-            db.item_owners.insert({ username: user.username, item_id: newListEvent[0].id })
-        ]);
-        await Promise.all([
-            db.users.insert(admin)
-        ]);
-
-        const listQuery = {
-            method: 'POST',
-            url: '/lists',
-            payload: list
+        token = JWT.sign({ id: user.id, username: user.username, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
+        query = {
+            method: 'DELETE',
+            headers: { 'Authorization': token }
         };
+    });
 
-        const token = JWT.sign({ id: user.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        listQuery.headers = { 'Authorization': token };
+    beforeEach(async () => {
 
-        await server.inject(listQuery)
-            .then((response) => {
-
-                newList = response.result.data;
-            });
-
-        const addItemQuery = {
-            method: 'POST',
-            headers: { 'authorization': token },
-            payload: { 'list_id' : `${ newList.id }`,
-                'item_id' : `${ newListEvent[0].id }` }
-        };
-
-        addItemQuery.url = `/lists/listitems`;
-
-        await server.inject(addItemQuery)
-            .then((response) => {
-
-            });
-
-
+        await Promise.all([
+            db.item_owners.destroy(),
+            db.items.destroy(),
+            db.lists.destroy(),
+            db.list_items.destroy()
+        ]);
     });
 
     after(async () => {
 
         await Promise.all([
-            db.items.destroy({ id: newMultiEvent[0].id }),
-            db.items.destroy({ id: newListEvent[0].id })
-        ]);
-        await Promise.all([
-            db.users.destroy({ id: user.id }),
-            db.users.destroy({ id: invalidUser.id }),
-            db.users.destroy({ id: admin.id })
-        ]);
-        await Promise.all([
-            db.lists.destroy({ id: newList.id })
+            db.users.destroy(),
+            db.item_owners.destroy(),
+            db.items.destroy(),
+            db.lists.destroy(),
+            db.list_items.destroy()
         ]);
     });
 
-    it('Destroy item', () => {
+    it('Destroy item', async () => {
 
-        const token = JWT.sign({ id: user.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        const payload = { username: user.username };
-        return server.inject({ method: 'delete', url: `/items/${ newEvent[0].id }`, payload, headers: { 'Authorization': token } }).then((res) => {
-
-            expect(res.statusCode).to.equal(204);
-        });
+        const item = await db.items.insert(Fixtures.place());
+        await db.item_owners.insert({ username: user.username, item_id: item.id });
+        query.url = `/items/${item.id}`;
+        const response = await server.inject(query);
+        expect(response.statusCode).to.equal(204);
     });
 
-    it('Invalid user destroy item', () => {
+    it('Invalid user destroy item', async () => {
 
-        const token = JWT.sign({ id: invalidUser.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        const payload = { username: user.username };
-        return server.inject({ method: 'delete', url: `/items/${ newListEvent[0].id }`, payload, headers: { 'Authorization': token } }).then((res) => {
-
-            expect(res.statusCode).to.equal(401);
-        });
+        const item = await db.items.insert(Fixtures.place());
+        await db.item_owners.insert({ username: userTwo.username, item_id: item.id });
+        query.url = `/items/${item.id}`;
+        const response = await server.inject(query);
+        expect(response.statusCode).to.equal(401);
+        expect(response.result.message).to.equal(
+            'Not permitted to edit item'
+        );
     });
 
-    it('Destroy item with multiple owners', () => {
+    it('Destroy item with multiple owners', async () => {
 
-        const token = JWT.sign({ id: user.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        const payload = { username: user.username };
-        return server.inject({ method: 'delete', url: `/items/${ newMultiEvent[0].id }`, payload, headers: { 'Authorization': token } }).then((res) => {
-
-            expect(res.statusCode).to.equal(412);
-        });
+        const item = await db.items.insert(Fixtures.place());
+        await db.item_owners.insert({ username: user.username, item_id: item.id });
+        await db.item_owners.insert({ username: userTwo.username, item_id: item.id });
+        query.url = `/items/${item.id}`;
+        const response = await server.inject(query);
+        expect(response.statusCode).to.equal(412);
+        expect(response.result.message).to.equal(
+            'Cannot Delete Item with additional Owner'
+        );
     });
 
-    it('Non-user (admin) destroy item', () => {
+    it('Non-user (admin) destroy item', async () => {
 
-        const token = JWT.sign({ id: admin.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        const payload = { username: user.username };
-        return server.inject({ method: 'delete', url: `/items/${ newMultiEvent[0].id }`, payload, headers: { 'Authorization': token } }).then((res) => {
-
-            expect(res.statusCode).to.equal(412);
-        });
+        const item = await db.items.insert(Fixtures.place());
+        await db.item_owners.insert({ username: user.username, item_id: item.id });
+        const newToken = JWT.sign(
+            { id: adminUser.id, username: adminUser.username, timestamp: new Date() },
+            Config.auth.secret,
+            Config.auth.options
+        );
+        const newQuery = {
+            method: 'DELETE',
+            url: `/items/${item.id}`,
+            headers: {
+                Authorization: newToken
+            }
+        };
+        const response = await server.inject(newQuery);
+        expect(response.statusCode).to.equal(204);
     });
 
-    it('Item does not exist', () => {
+    it('Item does not exist', async () => {
 
-        const token = JWT.sign({ id: admin.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        const payload = { username: admin.username };
-        const nonItem = newEvent[0].id + 100;
-        return server.inject({ method: 'delete', url: `/items/${nonItem}`, payload, headers: { 'Authorization': token } }).then((res) => {
-
-            expect(res.statusCode).to.equal(404);
-        });
+        query.url = `/items/${Faker.random.uuid()}`;
+        const response = await server.inject(query);
+        expect(response.statusCode).to.equal(404);
+        expect(response.result.message).to.equal(
+            'Item not found'
+        );
     });
 
-    it('Item in a list', () => {
+    it('Item in a list', async () => {
 
-        const token = JWT.sign({ id: user.id, timestamp: new Date() }, Config.auth.secret, Config.auth.options);
-        const payload = { username: user.username };
-        return server.inject({ method: 'delete', url: `/items/${ newListEvent[0].id }`, payload, headers: { 'Authorization': token } }).then((res) => {
-
-            expect(res.statusCode).to.equal(412);
-        });
+        const item = await db.items.insert(Fixtures.place());
+        const list = await db.lists.insert(Fixtures.list());
+        await db.list_items.insert({ list_id: list.id, item_id: item.id });
+        await db.item_owners.insert({ username: user.username, item_id: item.id });
+        query.url = `/items/${item.id}`;
+        const response = await server.inject(query);
+        expect(response.statusCode).to.equal(412);
+        expect(response.result.message).to.equal(
+            'Unable to delete item. Item in a list.'
+        );
     });
 });
