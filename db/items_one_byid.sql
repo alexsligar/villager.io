9a9d847b082c94475205c8bb0a80cee71377d005
Alex Sligar <info@gravelroad.co>
2019-01-17 13:01:54 -0800

    Clean up syntax"

--- db/items_one_byid.sql
+++ db/items_one_byid.sql
@@ -1,6 +1,8 @@
-Select items.*, array_agg(distinct item_tags.tag_name) as tags, array_agg(distinct item_owners.username) as owners
-From items 
-left join item_tags on items.id = item_tags.item_id
-left join item_owners on items.id = item_owners.item_id
+SELECT items.*, 
+array_agg(distinct item_tags.tag_name) AS tags, 
+array_agg(distinct item_owners.username) AS owners
+FROM items 
+LEFT JOIN item_tags ON items.id = item_tags.item_id
+LEFT JOIN item_owners ON items.id = item_owners.item_id
 WHERE items.id = ${id}
-Group by items.id
+GROUP BY items.id
